f96376ab545d9d3714a53cf164228fadd33068b8
MAHESH <maheshshinde9100@gmail.com>
2025-07-13 12:28:34 +0530

    Optimized approach using binary search for finding N trailing zeroes in factorial

--- src/FindNZeroesInFactorial.java
+++ src/FindNZeroesInFactorial.java
@@ -4,32 +4,61 @@ public class FindNZeroesInFactorial {
         System.out.println(search(n));
     }
     static int search(int n){
-        long fact = 1;
-        int i = 1;
-        while(true){
-            fact = fact*i;
-            if(getCount(fact,0) == n){
-                System.out.println(fact);
-                return i;
+//        long fact = 1;
+//        int i = 1;
+//        while(true){
+//            fact = fact*i;
+//            if(getCount(fact,0) == n){
+//                System.out.println(fact);
+//                return i;
+//            }
+//            i++;
+//        }
+
+        int start = 0;
+        int end = 5 * n;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = (start+end) / 2;
+            int zeroCount = getCount2(mid);
+
+            if (zeroCount == n) {
+                result = mid;
+                end = mid - 1;
+            } else if (zeroCount < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            i++;
         }
+
+        return result;
     }
 
-    static int getCount(long num,int count){
-        if(num<10){
-            if(num==0){
-                count++;
-            }
-            return count;
-        }
-        int rem =(int) num%10;
-        if(rem!=0){
-            return count;
-        }else{
-            count++;
-        }
-        return getCount(num/10,count);
-    }
+//    static int getCount(long num,int count){
+//        if(num<10){
+//            if(num==0){
+//                count++;
+//            }
+//            return count;
+//        }
+//        int rem =(int) num%10;
+//        if(rem!=0){
+//            return count;
+//        }else{
+//            count++;
+//        }
+//        return getCount(num/10,count);
+//    }
 
+    static int getCount2(int num)
+    {
+        int count = 0;
+        while(num > 0){
+            num = num / 5;
+            count =count+num;
+        }
+        return count;
+    }
 }
