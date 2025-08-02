f1293b252b8a5efdb1058a8f662697fb510dc77b
MAHESH <maheshshinde9100@gmail.com>
2025-08-02 18:27:04 +0530

    completed with Divide and Conquer 4th problem statement - ADSA

--- src/tasks/KthLargestElementInArray_04.java
+++ src/tasks/KthLargestElementInArray_04.java
@@ -1,11 +1,23 @@
 package tasks;
 
 import java.util.Arrays;
-
-public class SortAnArray_01 {
+/*test case:
+    input : [3,2,5,6,1]   k=2
+    output: 5
+*/
+public class KthLargestElementInArray_04 {
     public static void main(String[] args) {
-        int[] arr = new int[]{100,3,1,6,39,29,3,-1,4,0};
-        System.out.println(Arrays.toString(mergeSort(arr)));
+        int[] arr = new int[]{3,2,5,6,1};
+        int k = 2;
+        System.out.println(getKthLargestElement(arr,k));
+    }
+    static int getKthLargestElement(int[] arr,int k){
+        int[] res = mergeSort(arr);
+        int correctIndex = res.length-k;
+//        if(k<arr.length && res[correctIndex] == res[correctIndex+1]){
+//            return res[correctIndex-1];       //if 2 duplicates found consecutive
+//        }
+        return res[correctIndex];
     }
     private static int[] mergeSort(int[] arr){
         if(arr.length==1){
@@ -17,11 +29,9 @@ public class SortAnArray_01 {
         return merge(arr,left,right);
     }
     private static int[] merge(int[] arr,int[] left,int[] right){
-        int i=0;
-        int j=0;
-        int k=0;
+        int i=0,j=0,k=0;
         while(i<left.length && j<right.length){
-            if(left[i]<right[j]){
+            if(left[i] < right[j]){
                 arr[k] = left[i];
                 i++;
             }else{
