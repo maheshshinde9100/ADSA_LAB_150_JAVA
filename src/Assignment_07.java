6c0135a933759a2982879b0bdc637ea2776cafb4
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-09-01 14:31:31 +0530

    Completed assignment no. 7 --greedy approach

--- src/Assignment_07.java
+++ src/Assignment_07.java
@@ -1,22 +1,36 @@
 public class Assignment_07 {
     public static void main(String[] args) {
         int D = 18;
-        int[] task = new int[]{9,10,12,9};
-        float sum = 0;
-        for(int i=0;i<task.length;i++){
-            sum+= task[i];
+        int[] task = new int[]{9, 10, 12, 9};
+        int sum = 0;
+
+        for(int t:task) {
+            sum += t;
         }
-        float minHours = getMinHours(sum,D);
-        float maxDays = getMaxDays(minHours,sum);
 
-        System.out.println("Min Hours : "+minHours);
-        System.out.println("MAX Days : "+maxDays);
+        float minHours = Float.MAX_VALUE;
+        int maxDays = Integer.MIN_VALUE;
 
+        for(int i=1;i<=D;i++){
+            float hoursPerDay = getMinHours(sum, i);
+            minHours = Math.min(minHours, hoursPerDay);
+
+            int days = getMaxDays(sum,i);
+            if(days<=D){
+                maxDays = Math.max(maxDays, days);
+            }
+        }
+
+        System.out.println("Total Hours : " + sum);
+        System.out.println("Min Hours/day : " + minHours);
+        System.out.println("Max Days : " + maxDays);
     }
-    static float getMinHours(float sum,float D){
-        return (float) sum/D;
+
+    static float getMinHours(int totalHours, int days) {
+        return (float) totalHours /days;
     }
-    static float getMaxDays(float minHours,float sum){
-        return (float) sum/minHours;
+
+    static int getMaxDays(int totalHours,int hoursPerDay) {
+        return (int) Math.ceil((float) totalHours / hoursPerDay);
     }
 }
